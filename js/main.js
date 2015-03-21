9d31c6d21e55dc75ff14ca3e44c014b2476958c5
Muhammed Najmul Huda <del.rony@yahoo.com>
2015-03-21 23:01:03 +0100

    adding help text. Stop starting game after loading

--- js/main.js
+++ js/main.js
@@ -7,13 +7,25 @@ var Game = (function() {
         movingBar.moveBar();
     }
     
-    initAndStart();
+    //initAndStart();
     
-    $( "#control button").click(function() {
+    // HANDLE THE START BUTTON
+    $( "#control button.start" ).click(function() {
         if (movingBar.isMoving()) {
             movingBar.stopBar();
         }
         
         initAndStart();
+    });
+    
+    // HANDLE THE HELP BUTTON
+    $( "#control button.help" ).click(function() {
+        if ($( this ).text() === "Show help") {
+            $( "#help-text ").show();
+            $( this ).text("Hide help");
+        } else {
+            $( "#help-text ").hide();
+            $( this ).text("Show help");
+        }
     });
 })();
